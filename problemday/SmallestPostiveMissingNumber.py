3adf18e3cd9c28ed2d7efa503d35d6a45e09b953
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-07-09 13:19:13 +0530

    Smallest  Kth  postive missing number in arrray

---
+++ problemday/SmallestPostiveMissingNumber.py
@@ -0,0 +1,24 @@
+#naviv approch
+arr = {-1,1,2,3,4,5}
+sorted(arr)
+for i in range(1,10):
+    if i not in  arr:
+        print(i)
+
+        break
+#kth missing number
+K=15
+A={1,2,4,5,6,19}
+N=max(A)
+
+l=[]
+count=K
+
+for i in range(1,N):
+     if i not in A:
+         K=K-1
+         l.append(i)
+         if(K==0):
+             break
+print(l)
+print(l[K-1])
--- strings_file/findnoofoccuerence.py
+++ strings_file/findnoofoccuerence.py
@@ -8,4 +8,30 @@ for ch in s:
 
 for ch in sorted(l):
     print('{} occurs {} times'.format(ch, s.count(ch)))
- #secondway dictonary
+# secondary dictionary
+
+d = {'A': 100, 'B': 300}
+d.get('A')
+print(d)
+print(d.get('x'))
+print(d.get('x',220))
+d['A']=d.get('A',2)+1
+print(d)
+
+#printing for each items in list
+
+for K,V  in d.items():
+    print(K,V)
+#back to program
+
+
+s="hrfhewuirweufuhewrhw"
+result=''
+d={}
+for ch in s:
+    d[ch]=d.get(ch,0)+1
+
+for K,V in sorted(d.items()):
+    print(K,V)
+    result=result+str(V)+K
+print(result.title())
