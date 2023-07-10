a072f5e67453ca268a6fc42b89a884793b0016a0
DESKTOP-KNARGEO\Mobile Programming <pankaj.dimri@mobile.programming.com>
2023-07-10 15:57:56 +0530

    Remove element from arrrays

---
+++ arrays/remove_element_array.py
@@ -0,0 +1,23 @@
+A = [2, 3, 4, 5]
+N = len(A)
+# Last Elemt
+lastElement = N - 1
+print(A[lastElement])
+# display array
+
+for i in range(0, N):
+    print("ITEM:", i)
+sumOfElement = 0
+# summ of array element
+A = [2, 3, 4, 5,"3232333"]
+for k in range(0,N):
+    sumOfElement = sumOfElement+A[k]
+print("sumOfElement is :", sumOfElement)
+
+# Reverse the array
+print(A[::-1])
+
+while i<N:
+    print(A[i])
+    i=i-1
+"Remove element from array"
